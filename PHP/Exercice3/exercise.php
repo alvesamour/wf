a8934ac6f004b880caefb8cdd7585b016f41217f
alvesamour <vic_alves@hotmail.fr>
2018-10-19 12:39:01 +0200

    exercice 3 done with list

--- PHP/Exercice3/exercise.php
+++ PHP/Exercice3/exercise.php
@@ -2,3 +2,26 @@
 
 $input;
 $winner;
+
+$brotherA = 0;
+$brotherB = 0;
+
+foreach ($input as $player) {
+    //extract value and put in the same line
+    //list — Assigne des variables comme si elles étaient un tableau
+   list($brotherA, $brotherB) = $player;
+   
+   if ($brotherA > $brotherB) {
+       $resultatA++;
+   } else if ($brotherB > $brotherA){
+       $resultatB++;
+   }
+}
+    
+if ($resultatA > $brotherB) {
+    $winner = 'A';
+}else if ($resultatB > $brotherA){
+    $winner = 'B';
+}
+
+echo $winner;
