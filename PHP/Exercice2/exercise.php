ede71be82a12e2ce1c9c8e453211fade85d9c193
alvesamour <vic_alves@hotmail.fr>
2018-10-19 10:48:17 +0200

    exercice 2 correction

--- PHP/Exercice2/exercise.php
+++ PHP/Exercice2/exercise.php
@@ -2,4 +2,8 @@
 $password;
 $salt;
 
-$saltedPassword = '';
+$partOne = substr($password, 0, floor(strlen($password) / 2) + (strlen($password) % 2));
+$partEnd = substr($password, ceil(strlen($password)/2));
+$saltedPassword = $partOne.$salt.$partEnd;
+
+
