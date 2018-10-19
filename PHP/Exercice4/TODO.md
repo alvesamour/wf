a5fa577d06ad7f053537df60d22457ca67955bbc
alvesamour <vic_alves@hotmail.fr>
2018-10-19 15:47:33 +0200

    Exercice monday done

--- PHP/Exercice4/TODO.md
+++ PHP/Exercice4/TODO.md
@@ -7,3 +7,9 @@ The returning date is expected to be a string, and to follow the following forma
 The function MUST be named 'getAllMondaysOfMonth'.
 
 To validate it, just cd into the Exercice2 folder and run "php phpunit-6.5.5.phar".
+
+Pour préparer une réunion, votre CTO souhaite une fonction renvoyant tous les lundis du mois sous forme de chaîne dans un tableau. La fonction recevra au premier argument une année et à la deuxième place, un mois, tous deux de type entier.
+
+La date de retour doit être une chaîne et suivre le format suivant: «Lundi 1er février 2021».
+
+La fonction DOIT être nommée 'getAllMondaysOfMonth'.
--- PHP/Exercice4/exercise.php
+++ PHP/Exercice4/exercise.php
@@ -1,1 +1,24 @@
 <?php
+
+function getAllMondaysOfMonth($anne, $mois){
+    $mondays = [];
+    $date = DateTime::createFromFormat('Yn', $anne.$mois);
+    //F	Mois, textuel, version longue; en anglais, comme January ou December	January à December
+    $date = new DateTime('first day of '.$date->format('F Y'));
+    
+    $interval = DateInterval::createFromDateString('next monday');
+    //N	Représentation numérique ISO-8601 du jour de la semaine (ajouté en PHP 5.1.0)
+    if ($date->format('N') != 1) {
+        $date->add($interval);
+    }
+    //m	Mois au format numérique, avec zéros initiaux	01 à 12
+    while ($date -> format('m') == $mois) {
+        //l ('L' minuscule)	Jour de la semaine, textuel, version longue, en anglais
+        //j	Jour du mois sans les zéros initiaux	1 à 31
+        //M	Mois, en trois lettres, en anglais	Jan à Dec
+        //Y	Année sur 4 chiffres	Exemples : 1999 ou 2003
+        $mondays[] = $date -> format('l j, M Y') ;
+        $date -> add($interval);
+    }
+    return $mondays;
+}
