ce0218d23859dba2504718753dca46db19902e48
alvesamour <vic_alves@hotmail.fr>
2018-10-25 10:34:39 +0200

    model

---
+++ PHP/tagBeSill/model/Articles.php
@@ -0,0 +1,21 @@
+<?php 
+    //Data loading model for articles
+    //Articles : id, pub_date, img, title, description
+    //TODO: Currently hardcoded have to come from the database
+    //FIXME: Return articles instead of variable declaration
+    $articles = [
+        [
+            'id' => 1,
+            'pub_date' => '2018-06-21 11:43:12',
+            'img' => 'https://picsum.photos/100/100',
+            'title' => 'Title 1',
+            'description' => 'Lorem ipsum dolor sit amet, idque assentior et nam, viris corrumpit ius eu.'
+        ],
+        [
+            'id' => 2,
+            'pub_date' => '2018-06-22 09:33:17',
+            'img' => 'https://picsum.photos/g/100/100',
+            'title' => 'Title 2',
+            'description' => 'Lorem ipsum dolor sit amet, idque assentior et nam, viris corrumpit ius eu. Dico stet essent pro te, autem nostrud noluisse at eum.'
+        ]
+    ];
