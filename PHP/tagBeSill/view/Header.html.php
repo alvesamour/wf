e2a65fb7cba5beaf9529324d5983953c0bf3050d
alvesamour <vic_alves@hotmail.fr>
2018-10-26 14:22:17 +0200

    done prof1

---
+++ PHP/tagBeSill/view/Header.html.php
@@ -0,0 +1,16 @@
+<header class="container mb-6">
+	<div class="row">
+    	<div class="col-sm-4">
+    		<img alt="Logo" src="/img/logo.png" class="img-fluid">
+    	</div>
+    	<div class="col align-self-end">
+    		<h1>
+    			<strong class="fs-important">
+    				<span class="c-red">Tag</span><span class="c-green">Be</span><span class="c-blue">Sill</span>
+    			</strong>
+    			<span class="fs-05">Project management system</span>
+    		</h1>
+    	</div>
+	</div>
+	<hr>
+</header>
