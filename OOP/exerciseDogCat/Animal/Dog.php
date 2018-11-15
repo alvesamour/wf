8b7f2e44204c0c1deb985f8c1b9fd3b4cadb7801
Romilson Alves <vic_alves@hotmail.fr>
2018-11-15 16:48:19 +0100

    complete

--- OOP/exerciseDogCat/Animal/Dog.php
+++ OOP/exerciseDogCat/Animal/Dog.php
@@ -1,97 +1,22 @@
 <?php 
 namespace Animal;
 
-class Dog {
-    public const DOG_NAME = 'Ding';
-    public const DOG_RACE = ['Pitbull', 'Husky'];
-    public const DOG_COLOR = ['black', 'White'];
-    
-    private $name;
-    private $race;
-    private $bodyColor;
-    
-    public function __construct(string $name, string $race, string $bodyColor) {
-        $this->setName($name)
-        ->setRace($race)
-        ->setBodyColor($bodyColor);
+class Dog extends Animal implements PetInterface
+{
+    public function bark() {
+        return 'Wafff';
     }
     
-   
-    /**
-     * @return mixed
-     */
-    public function getName()
-    {
-        return $this->name;
-    }
-
-    /**
-     * @return mixed
-     */
-    public function getRace()
-    {
-        return $this->race;
-    }
-
-    /**
-     * @return mixed
-     */
-    public function getBodyColor()
-    {
-        return $this->bodyColor;
-    }
-
-    /**
-     * @param mixed $name
-     */
-    public function setName($name)
-    {   
-        if ($name != self::DOG_NAME) {
-        Throw new \RuntimeException('Bad name');   
-        }
-        $this->name = $name;
-        return $this;
-    }
-
-    /**
-     * @param mixed $age
-     */
-    public function setRace($race)
-    {
-        if (!in_array($race, self::DOG_RACE)) {
-            Throw new \RuntimeException('it\'s not a race');
-        }
-        
-        $this->race = $race;
-        return $this;
-    }
-
-    /**
-     * @param mixed $bodyColor
-     */
-    public function setBodyColor($bodyColor)
-    {
-        if (!in_array($bodyColor, self::DOG_COLOR)) {
-            Throw new \RuntimeException('it\'s not a color of dog');
-        }
-        $this->bodyColor = $bodyColor;
-        return $this;
-    }
-
-    public function drink()
-    {
-        echo 'The Dog drink<br/>';
+    public function eat() {
+        return 'Eat well';
     }
     
-    public function eat()
-    {
-        return 'The Dog eat<br/>';
+    public function drink() {
+        return 'Drink wather';
     }
     
-    public function bark(){
-        return "Woof! Woof!";
+    public function eat($whet)
+    {
+        return 'Need go out';
     }
-
 }
-
-
--- OOP/exerciseDogCat/main.php
+++ OOP/exerciseDogCat/main.php
@@ -1,6 +1,7 @@
 <?php 
-//require_once 'Animal/Cat.php';
-//require_once 'Animal/Dog.php';
+
+use Animal\Animal;
+use Animal\PetInterface;
 
 spl_autoload_unregister(function ($namespace) {
     $path = str_replace('\\', DIRECTORY_SEPARATOR, $namespace);
@@ -11,15 +12,10 @@ spl_autoload_unregister(function ($namespace) {
     }
 });
 
-use Animal\Dog;
-use Animal\Cat;
-
-$dog = new Dog('Ding', 'Husky', 'White');
-$cat = new Cat();
-
-
-echo sprintf(
-    'The dog Say %s when the cat say %s',
-    $dog->bark(),
-    $cat->purr()
-);
+    function tryBark(Animal $animal){
+        return $animal->bark();
+    }
+    
+    function tryEat(PetInterface $animal){
+        return $animal->eat('Beef');
+    }
